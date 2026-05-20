476c6b8992ec7d6df138392a625bf47bf0140b96
Nguyen Huynh Kim Suong <ksuong2211@gmail.com>
2026-05-20 16:01:20 +0700

    Implement nextGreaterElement function

---
+++ week3/Next-Greater-Element-I.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let ans = [];
+    for (let i = 0; i < nums1.length; i++) {
+        let found = false;
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] === nums2[j]) {
+                for (let k = j; k < nums2.length; k++) {
+                    if (nums2[j] < nums2[k]) {
+                        ans.push(nums2[k]);
+                        found = true;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        if (!found) ans.push(-1);
+    }
+    return ans;
+};
