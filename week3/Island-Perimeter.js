5505966cbf6e8810d80d651b66dfd1bef3ef25e4
Nguyen Huynh Kim Suong <ksuong2211@gmail.com>
2026-05-22 12:57:54 +0700

    Implement island perimeter calculation function

---
+++ week3/Island-Perimeter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function(grid) {
+    let perimeter = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) perimeter += 4;
+            if (grid[i][j] === 1 && j + 1 < grid[i].length && grid[i][j + 1] === 1) {
+                perimeter -= 2;
+            }
+            if (grid[i][j] === 1 && i + 1 < grid.length && grid[i + 1][j] === 1) {
+                perimeter -= 2;
+            }
+        }
+    }
+    return perimeter;
+};
